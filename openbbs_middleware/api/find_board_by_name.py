3abcdb93e60ce013acd907caf23bf712066519e0
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2020-11-28 15:50:56 -0500

    get_favorite_board and find_board_by_name

--- openbbs_middleware/api/find_board_by_name.py
+++ openbbs_middleware/api/find_board_by_name.py
@@ -3,4 +3,17 @@
 
 def find_board_by_name(params):
     '''XXX fake-data'''
-    return None, [{'bid': 'PttNewhand', 'title': '批踢踢新手客服中心… 〃非test板', 'flag': 1, 'cat': '新手', 'nuser': 100, 'bm': ['teemo', 'okcool']}]
+    result = {
+        'list': [
+            {
+                'bid': 'PttNewhand',
+                'title': '批踢踢新手客服中心… 〃非test板',
+                'flag': 1,
+                'cat': '新手',
+                'nuser': 100,
+                'bm': ['teemo', 'okcool'],
+            },
+        ],
+        'next': '',
+    }
+    return None, result
