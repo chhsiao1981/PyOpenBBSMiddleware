977fd3290c5e36f8ac624082652ed281dbaa7d84
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2020-11-29 12:45:24 -0500

    setup host as 173.255.216.176:3457

--- .cc/scripts/swagger.sh
+++ .cc/scripts/swagger.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-flaskswagger openbbs_middleware.main:app --host localhost:5000 --base-path / --out-dir swagger --from-file-keyword=swagger_from_file --template ./apidoc/template.json
+flaskswagger openbbs_middleware.main:app --host 173.255.216.176:3457 --base-path / --out-dir swagger --from-file-keyword=swagger_from_file --template ./apidoc/template.json
 
 docker container stop swagger-ui
 docker container rm swagger-ui
